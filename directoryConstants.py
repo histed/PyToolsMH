885d920d6137a70cd0ca02d815fcde33366405e5
Mark Histed <mhisted@gmail.com>
2013-06-13 18:29:41 -0400

    small change for 10.8

--- directoryConstants.py
+++ directoryConstants.py
@@ -5,7 +5,7 @@ import os, string
 (system, hostname, kernVersion, fullUname, arch) = os.uname()
 
 hostname = string.replace(hostname, '.local', '')
-if hostname == 'MaunsellMouse1':
+if hostname == 'Maunsell108Mouse1':
     varSyncDir = '/Users/holdanddetect/MWVariables-backups';
 elif hostname == 'mambo':
     varSyncDir = '/Users/histed/data/mus-behavior/MWVariables-backups';
