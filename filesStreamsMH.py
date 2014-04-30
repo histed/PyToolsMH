db4be0473d0f02830cf03cf16e7d3c77a27033e0
Mark Histed <mhisted@gmail.com>
2014-04-30 09:57:40 -0400

    Add TeeStreamsBlockable: send output from one stream to two (like 'tee') - also allows blocking/unblocking the streams (discarding their output temporarily)

--- filesStreamsMH.py
+++ filesStreamsMH.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 
 class StreamToLogger(object):
    """
@@ -19,4 +20,32 @@ class StreamToLogger(object):
        # no need to do anything on flush because logging is synchronous
        pass
 
- 
+
+class TeeStreams(object):
+   """Send output for one stream to multiple streams, like 'tee'"""
+   _streamList = None
+   def __init__(self, stream1, stream2):
+      self._streamList = []
+      self._streamList.append(stream1)
+      self._streamList.append(stream2)
+   def write(self, data):
+      for s in self._streamList:
+         if s is not None:
+            s.write(data)
+
+class TeeStreamsBlockable(TeeStreams):
+   """Extends TeeStreams to allow blocking/unblocking streams (temporarily discarding their output)"""
+   _streamListOrig = None
+   def __init__(self, stream1, stream2):
+      super(TeeStreamsBlockable, self).__init__(stream1, stream2)
+      self._streamListOrig = copy.copy(self._streamList)
+   def blockStream(self, streamN):
+      if streamN < 0 or streamN >= len(self._streamList):
+         raise RuntimeError, ('Unknown stream number %d' % streamN)
+      self._streamList[streamN] = None
+   def unblockStream(self, streamN):
+      if streamN < 0 or streamN >= len(self._streamList):
+         raise RuntimeError, ('Unknown stream number %d' % streamN)
+      self._streamList[streamN] = self._streamListOrig[streamN]
+
+
