019410a313807b533d869158c9ddf75ffdc6f805
Mark Histed <mhisted@gmail.com>
2015-08-11 10:45:17 -0500

    refactor from modules into a package
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+# Utility function to read the README file.  Used for the long_description.  better to use the file than to put a raw string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "PytoolsMH",
-    version = "0.0.2",
+    name = "pytoolsMH",
+    version = "0.0.3",
     author = "Mark Histed",
     author_email = "mhisted@gmail.com",
-    description = ("A miscellaneous set of python utilities, mainly for neural data analysis"),
+    description = ("misc personal Python utilities"),
     license = "none",
     keywords = "",
     url = "", 
-    py_modules=['behaviorLibraryMH', 'mathMH', 'windowsTimingMH', 'macTimingMH', 'crossPlatformMH', 'filesStreamsMH', 'plotMH', 'containersMH', 'memProfileMH' ],
-    #packages=['an_example_pypi_project', 'tests'],
+    packages=['pytoolsMH'],
+    package_dir={'pytoolsMH': 'pytoolsMH'},
     long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
     ],
 )
 
+# 0.0.3: convert to package
 # 0.0.2: add plotMH
 # 0.0.1
