4df561a552e52c1229115c194e008ca0b557e618
Mark Histed <mhisted@gmail.com>
2017-09-14 16:41:30 -0400

    resolve conflicts when rebasing

--- pytoolsMH/windowsTiming.py
+++ pytoolsMH/windowsTiming.py
@@ -1,8 +1,16 @@
-
-# histed 120724
+# windowsTiming histed 120724
 #
-# mostly copied from pyglet:
+# To reduce interrupt latency / min sleep time to 1ms in windows: call timeBeginPeriod(1)   - default is 10ms.
+
+# As of 170914: this still works on win 7 to decrease sleep time: before 10ms, after 1ms
+#  2017-09-12 10:35:47,084 Testing minimum sleep time before and after reset:
+#  2017-09-12 10:35:48,094 Before: 9.9ms
+#  2017-09-12 10:35:49,095 After: 1.0ms
+
+# Timer interrupt stuff mostly copied from pyglet in 2009 (long since changed in pyglet).
 #   http://code.google.com/p/pyglet/issues/attachmentText?id=445&aid=-2288975315691413876&name=windowstimer.py&token=68bf0416226af9080cf91ec9aa168356
+#
+
 
 import time
 import psutil
@@ -25,7 +33,7 @@ class TIMECAPS(Structure):
 caps = TIMECAPS()
 timeGetDevCaps(byref(caps), sizeof(caps))
 
-def timerTest(resolution, duration):
+windll.winmdef timerTest(resolution, duration):
     start = time.clock()
     end = start + duration
     now = start
