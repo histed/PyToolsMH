c24156bd10b0d9cc8ebad4c010f64e1d4813230b
Mark Histed <mhisted@gmail.com>
2018-01-16 15:13:44 -0500

    move image to the try-load in init

--- pytoolsMH/__init__.py
+++ pytoolsMH/__init__.py
@@ -5,7 +5,6 @@ from . import math
 from . import memProfile
 from . import plotColors
 from . import string
-from . import image
 import importlib
 
 from . import _version
@@ -14,7 +13,7 @@ __version__ = _version.__version__
 ## This module uses anaconda, and so assumes numpy, scipy etc are installed.
 
 # Don't load some modules if dependencies are missing
-for tN in ['plotting', 'dataio']:
+for tN in ['plotting', 'dataio', 'image']:
     try:
         globals()[tN] = importlib.import_module('.'+tN, __name__)
     except ImportError as e:
