dbe7e881991a8bd8e9e8bc94b6de85753d9103ad
Mark Histed <mhisted@gmail.com>
2018-05-09 15:54:34 -0400

    bump version
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- pytoolsMH/dataio.py
+++ pytoolsMH/dataio.py
@@ -3,6 +3,9 @@ import pandas as pd
 import tables
 import warnings
 from tables.nodes import filenode
+import inspect
+import argparse
+import numpy as np
 
 from six import iteritems,iterkeys,itervalues
 import dill
@@ -53,3 +56,47 @@ def readDataFromH5(fName):
         for tP in pklNames:
             initD[tP] = dill.loads(getattr(store._handle.root.objects, tP)[:])
     return initD
+
+## these functions need work: check for file already existing.  deal with non-normalizable var names in namespace.  etc?
+# needsa lso: global lookup, probably refactor lookup code into function that caches dicts...
+
+def save(filename, *args):
+    """Use the h5 format to save all kinds of objects.  See saveDataAsH5 for disk format.
+
+    Args:
+        filename: filename to save into (h5)
+        args: objects to save.  In file they will get same name as in calling namespace (if you don't know what that means, don't worry about it)
+    Returns:
+        None
+
+    Example: dataio.save('Desktop/outfile', array1, array2)
+    """
+    saveD = {}
+
+    # get name for each obj
+    callinglocals = inspect.currentframe().f_back.f_locals
+    keyA = np.array(list(callinglocals.keys()), dtype='O')
+    valA = np.array(list(callinglocals.values()), dtype='O')
+    for tA in args:
+        matchIx = valA == tA
+        nM = np.sum(matchIx)
+        if nM == 1:
+            tName = keyA[matchIx][0]
+        elif nM > 1:
+            raise RuntimeError('bug: more than one match??')
+        elif nM == 0:
+            raise RuntimeError('name not found in locals.  In globals()?  Add to this code and test')
+        saveD.update({tName:tA})
+
+    # write
+    saveDataAsH5(filename, saveD)
+
+
+def load(filename):
+    """read objects from h5 format.  Put them in a namespace var"""
+    tD = readDataFromH5(filename)
+    # if this throws an error, do something nice to clean it up...Named tuple?
+    return argparse.Namespace(**tD)
+
+
+
