5117a8eb7c76c2edde512252ad43ab7ba8881b9a
Mark Histed <mhisted@gmail.com>
2019-11-14 11:30:24 -0500

    git branch functions

--- pytoolsMH/paths.py
+++ pytoolsMH/paths.py
@@ -1,5 +1,20 @@
 import shutil
+# lazy load, dont' import git here, requires gitpython, present in mh37c envs et al
 
+
+def get_git_root(path):
+    """Given a directory or file, find the enclosing git repo path"""
+    import git
+    git_repo = git.Repo(path, search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return git_root
+
+
+def get_git_branch(path):
+    """Given a directory or file, find the branch of any enclosing git repo"""
+    import git
+    git_repo = git.Repo(path, search_parent_directories=True)
+    return git_repo.active_branch.name
 
 
 def check_min_gb(indir, mingb):
